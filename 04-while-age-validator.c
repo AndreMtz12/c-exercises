a9222bdd8f051b98375efac6a1c1b44f69faa0d9
AndreMtz12 <andre.mtz1210@gmail.com>
2025-12-01 17:42:13 +0000

    complete exercise 04

--- 04-while-age-validator.c
+++ 04-while-age-validator.c
@@ -18,6 +18,14 @@ int main(void) {
     int age = -1;
 
     // TODO: add your while loop here to validate the input.
+   while (age<0 || age>120)
+   {
+
+    printf("Please enter an age between 0 and 120: ");
+    scanf("%d", &age);
+   }
+
+   printf("You entered: %d", age);
 
     return 0;
 }
